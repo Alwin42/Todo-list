d859c765fe71a1905498e7ea7ec71b8dad0da030
Alwin42 <alwinemmanuel424@gmail.com>
2025-05-16 12:21:37 +0530

    UI and bug fixes

---
+++ script.js
@@ -0,0 +1,34 @@
+const todoInput = document.getElementById("todo-input");
+const listContainer = document.getElementById("list-items");
+
+function addTask()
+{
+    if(todoInput.value === "")
+    {
+        alert("Please enter a task.");
+        return;
+    }
+    else
+    {
+        let li =document.createElement("li");
+        li.innerHTML = todoInput.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    todoInput.value = "";
+
+}
+
+listContainer.addEventListener("click",function(e)
+{
+    if(e.target.tagName == "LI")
+    {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName == "SPAN")
+    {
+        e.target.parentElement.remove();
+    }
+},false);
